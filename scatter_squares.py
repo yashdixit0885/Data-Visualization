21d231d101f61cd8ca13b78733b058afbcb2cd27
Yash Dixit <yashdixit0885@gmail.com>
2022-08-01 16:09:11 -0600

    automated data generation in scatter plot example

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-input_values = [1,2,3,4,5] #input data set
-squares= [1,4,9,16,25] #Output data set
+input_values = range(1,100)
+squares= [x**2 for x in input_values]
 plt.style.use('seaborn')
 fig,ax = plt.subplots()
 
 
-ax.scatter(input_values,squares,s=100)
+ax.scatter(input_values,squares,s=10)
 
 ax.set_title("Square Numbers", fontsize=24) # Sets the title of the chart with fontsize parameter describing the size of the font
 ax.set_xlabel("Value", fontsize=14)
@@ -15,5 +15,7 @@ ax.set_ylabel("Square of Value", fontsize=14)
 #set size of tick labels
 
 ax.tick_params(axis='both', labelsize=14)# tick_params() method styles the tick marks on the graph for better view
+
+ax.axis([0,101,0,10001])
 
 plt.show()

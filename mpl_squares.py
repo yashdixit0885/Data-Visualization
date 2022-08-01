1124d5bf83332fa9de2fdb2b2a5ed0b48b68aa57
Yash Dixit <yashdixit0885@gmail.com>
2022-08-01 15:26:12 -0600

    added more styling in the graph

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,7 +4,18 @@ squares= [1,4,9,16,25]
 
 fig,ax = plt.subplots() # subplots() function can generate one or more plots in the same figure. The variable fig represents the entire figure whereas ax represents one plot in the figure
 
-ax.plot(squares) # We use ax to plot the squares on the figure
+ax.plot(squares, linewidth=3) # We use ax to plot the squares on the figure. Plot() method plots the graph in a meaningful way and linewidth parameter tells the thickness of the line
+
+# Set chart title and label axes
+
+ax.set_title("Square Numbers", fontsize=24) # Sets the title of the chart with fontsize parameter describing the size of the font
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#set size of tick labels
+
+ax.tick_params(axis='both', labelsize=14)# tick_params() method styles the tick marks on the graph for better view
+
 
 plt.show() # Show is used to open the Matplotlib's viewer and display the plot
 

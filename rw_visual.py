2663ba3d7a16c707fee13fe5e5492e6a81c30511
Yash Dixit <yashdixit0885@gmail.com>
2022-08-02 13:40:52 -0600

    added a loop to automatically run the walks

--- rw_visual.py
+++ rw_visual.py
@@ -2,13 +2,18 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#Make a random walk
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    #Make a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-#Plot the points in the walk
-plt.style.use('classic')
-fig,ax = plt.subplots()
+    #Plot the points in the walk
+    plt.style.use('classic')
+    fig,ax = plt.subplots()
 
-ax.scatter(rw.x_values,rw.y_values,s=15)
-plt.show()
+    ax.scatter(rw.x_values,rw.y_values,s=15)
+    plt.show()
+
+    keep_running = input("Make another walk?(y/n)")
+    if keep_running == 'n':
+        break

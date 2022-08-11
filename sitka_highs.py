23666227725d13d4affa1d9e26081f7179afb3b3
Yash Dixit <yashdixit0885@gmail.com>
2022-08-11 15:20:45 -0600

    added datetime code to display dates on the x-axis

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,11 @@
 
+
 import csv
+
+from datetime import datetime
+
+
+import matplotlib.pyplot as plt
 
 fname = 'Data Visualization\data\sitka_weather_07-2018_simple.csv'
 
@@ -11,13 +17,31 @@ with open(fname) as f: # Opens the file and assigns it to the object f
 # for index,column_header in enumerate(header_row): # enumerate() function returns both the index of each item and the value of each item as you loop through a list
     # print(index,column_header)
 
-# Get high temperatures from this file
+# Get dates and high temperatures from this file
 
-    highs =[]
+    dates,highs =[],[]
 
     for row in reader:
+        current_date = datetime.strptime(row[2],'%Y-%m-%d')
         high = int(row[5])
         highs.append(high)
+        dates.append(current_date)
         # print(row) # Prints all the data in the CSV
 
     print(highs)
+
+# Plot high temperatures
+
+    plt.style.use('seaborn')
+
+    fig,ax = plt.subplots() 
+    ax.plot(dates,highs, c = 'red') 
+    # Set chart title and label axes
+    ax.set_title("Daily High Temperatures, July 2018", fontsize=24) 
+    ax.set_xlabel('', fontsize=14)
+    fig.autofmt_xdate()
+    ax.set_ylabel("Temperatures(F)", fontsize=16)
+    #set size of tick labels
+    ax.tick_params(axis='both', which='major',labelsize=16)
+
+    plt.show()

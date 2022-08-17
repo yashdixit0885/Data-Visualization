d39aa8e25060ee130f3295de69dc9ca67d5c1dcd
Yash Dixit <yashdixit0885@gmail.com>
2022-08-17 16:41:14 -0600

    refactored code and downloaded live data to represent last one day of earthquakes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -6,8 +6,8 @@ from plotly import offline
 
 # Explore the structure of the data
 
-filename = 'Data Visualization/data/eq_data_30_day_m1.json'
-with open(filename) as f:
+filename = 'Data Visualization/data/oneday.json'
+with open(filename, encoding='utf-8') as f:
     all_eq_data = json.load(f)
     
 
@@ -18,15 +18,15 @@ with open(readable_file, 'w') as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags,lons,lats =[],[],[]
+all_eq_title = all_eq_data['metadata']['title']
+
+mags,lons,lats,hover_texts =[],[],[],[]
 
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag'] # In order to extract value from a nested dictionary, you put in the subkey after the main key
-    lon= eq_dict['geometry']['coordinates'][0]
-    lat= eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
 # Map the earthquakes
 
@@ -34,6 +34,7 @@ data = [{
     'type':'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     # marker attribute in the data is what displays the plots on the map and can be modified by different properties
     'marker':{
         'size':[5*mag for mag in mags],
@@ -43,7 +44,7 @@ data = [{
         'colorbar':{'title':'Magnitude'},
         }
     }]
-my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title=all_eq_title)
 
 fig = {'data':data,'layout':my_layout}
 offline.plot(fig,filename='global_earthquakes.html')

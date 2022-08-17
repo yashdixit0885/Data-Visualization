98b55549227269b4bca1da5d4a58b859d88b3cd0
Yash Dixit <yashdixit0885@gmail.com>
2022-08-17 10:34:16 -0600

    added colormap

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -6,7 +6,7 @@ from plotly import offline
 
 # Explore the structure of the data
 
-filename = 'Data Visualization/data/eq_data_1_day_m1.json'
+filename = 'Data Visualization/data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
     
@@ -30,7 +30,19 @@ for eq_dict in all_eq_dicts:
 
 # Map the earthquakes
 
-data = [Scattergeo(lon=lons,lat=lats)]
+data = [{
+    'type':'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    # marker attribute in the data is what displays the plots on the map and can be modified by different properties
+    'marker':{
+        'size':[5*mag for mag in mags],
+        'color': mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Magnitude'},
+        }
+    }]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data':data,'layout':my_layout}

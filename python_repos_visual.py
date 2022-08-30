37bd4bc404726d943593b4817acd33669ab8248f
Yash Dixit <yashdixit0885@gmail.com>
2022-08-30 10:28:12 -0600

    added styling code to visualization

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -17,26 +17,45 @@ response_dict = r.json()
 repo_dicts = response_dict['items']
 # print(f"Repositories returned:{len(repo_dicts)}")
 
-repo_names, stars = [],[]
+repo_links, stars,labels = [],[],[]
 
 # print("\nSelected information about each repo:")
 
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link= f"<a href='{repo_url}'>{repo_name}</a>"# We use the HTML anchortag, which has the form <a href = 'URL'>link text </a>, to generate the link.
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
+    labels.append(f"{repo_dict['owner']['login']}<br />{repo_dict['description']}")
 
 #Make Visualization
 
 data = [{
     'type':'bar',
-    'x':repo_names,
+    'x':repo_links,
     'y':stars,
+    'hovertext':labels,
+    'marker':{
+        'color':'rgb(60,100,150)',
+        'line':{'width':1.5,'color':'rgb(25,25,25)'}
+    },
+    'opacity':0.6,
 }]
 
 my_layout = {
     'title':'Most starred python projects on GitHub',
-    'xaxis':{'title':'Repository'},
-    'yaxis':{'title':'Stars'}
+    'titlefont':{'size':28},
+    'xaxis':{
+        'title':'Repository',
+        'titlefont':{'size':24},
+        'tickfont':{'size':14}
+        },
+    'yaxis':{
+        'title':'Stars',
+        'titlefont':{'size':24},
+        'tickfont':{'size':14}
+    }
     }
 
 fig = {'data':data,'layout':my_layout}
